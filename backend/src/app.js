2ca4ca678a2be0116c5543001c1819fff618c42a
JALAJ MISHRA <mjalaj77@gmail.com>
2023-08-17 00:57:25 +0530

    Depreciation working started

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,7 @@ const { set } = require('mongoose')
 const { validateHeaderValue } = require('http')
 const port = process.env.PORT || 3000
 const cors = require("cors")
+const { count } = require('console')
 app.use(express.static("../public"))
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
@@ -169,6 +170,70 @@ app.get('/fetchassets', async(req, res) => {
   });
 
 
+
+//Depreciation calculation.
+app.post('/depreciation', async(req, res) => {
+    // Set response header for CSV download
+    const data = await assetRegister.find({})
+    
+    const count = await assetRegister.countDocuments();            // to count the total number of rows in the collection.
+    // console.log(count)    
+    
+    for(i=0;i<count;i++){
+
+        const asset_life = data[i]["assetLife"]
+        const dep_percent = 100/asset_life
+        console.log("Depreciation percentage is :", dep_percent,"%")
+
+        const assetCp = data[i]["costPrice"]
+        console.log("Asset cost price is :", assetCp,"rs.")
+        
+        const assetPd = new Date(data[i]["purchaseDate"])
+        // console.log(apd)
+        const tillDate = new Date(req.body.tilldate)
+        // console.log(td)
+        const dates_Diff = tillDate - assetPd
+        const dep_Days = Math.floor(dates_Diff / (1000 * 60 * 60 * 24));
+        console.log("Number of days in which the asset is depreciated are :", dep_Days)
+
+        const dep_Amount = (dep_Days*dep_percent*assetCp)/100
+        console.log("Depreciation amount for this asset will be :", dep_Amount,"rs.")
+
+        
+    }
+    
+
+    res.setHeader('Content-Type', 'text/csv');
+    res.setHeader('Content-Disposition', 'attachment; filename=data.csv');
+  
+    // Create CSV writer
+    const csvWriter = createCsvWriter({
+      path: 'data.csv',
+      header: [
+        { id: 'Category', title: 'Category' },
+        { id: 'assetId', title: 'Id' },
+        { id: 'assetName', title: 'Name' },
+        { id: 'purchaseDate', title: 'pDate' },
+        { id: 'aQty', title: 'Quantity' },
+        { id: 'costPrice', title: 'CostPrice' },
+        { id: 'assetLife', title: 'Usefull_Life' },
+        { id: 'purchaseReason', title: 'PReason' },
+        { id: 'dep_percent', title: 'dep_%' },
+        { id: 'dep_Amount', title: 'dep_Amt.' },
+      ],
+    });
+  
+    // Write the data to CSV
+    csvWriter.writeRecords(data)
+      .then(() => {
+        console.log('CSV file created successfully');
+        res.sendFile('data.csv', { root: __dirname });
+      })
+      .catch(error => {
+        console.error('Error creating CSV file:', error);  
+        res.status(500).send('Error creating CSV file');
+      });
+  });
 
 // Port Listening 
 app.listen(port, ()=>{
