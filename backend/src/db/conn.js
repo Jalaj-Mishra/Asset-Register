c1cbabbe0391d5d77caba2e11a3ac4c42bfe81bb
JALAJ MISHRA <125948938+Jalaj-Mishra@users.noreply.github.com>
2023-08-25 22:55:10 +0530

    Update conn.js

--- backend/src/db/conn.js
+++ backend/src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://0.0.0.0:27017/registration", {
+mongoose.connect("mongodb+srv://mjalaj77:jalajhostingdb@cluster0.vvxtrbj.mongodb.net/", {
     useNewUrlParser:true,
     useUnifiedTopology:true,
 }).then(() => {
